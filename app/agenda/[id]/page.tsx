7b0cd0d16aad7f2f087117f56370c33f23b09810
Hemira BOGNI <hemira.bogni@davinciitsolutions.fr>
2026-02-06 10:25:30 +0100

    modification AgendaMedecin.tsx

--- app/agenda/[id]/page.tsx
+++ app/agenda/[id]/page.tsx
@@ -1,9 +1,5 @@
 import AgendaMedecin from "../../../components/Doctor/AgendaMedecin";
 
-// Next.js App Router attend { params }
 export default function Page({ params }: { params: { id: string } }) {
-  // On force en string pour éviter le type number / string
-  const medecinId = params.id;
-
-  return <AgendaMedecin medecinId={medecinId} />;
+  return <AgendaMedecin medecinId={params.id} />;
 }
